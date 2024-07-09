16f1add038ef8f8f21acadaecfca43d471b141d9
Amit Singhal <rakshit.singhal712@gmail.com>
2024-07-09 22:34:43 +0530

    Get Small Cups & Big Cup

---
+++ script.js
@@ -0,0 +1,4 @@
+const smallCups = document.querySelectorAll(".cup-sm");
+const liters = document.getElementById("liters");
+const percentage = document.getElementById("percentage");
+const remained = document.getElementById("remained");
